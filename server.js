b0b054a0bfd5112402864ad716c04fc349873966
Sergio <smatemartin@hotmail.com>
2020-06-19 23:32:44 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// EXPRESS HBS ENGINE
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        nombre: 'sergio mAte martíN'
+    });
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs');
+})
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
